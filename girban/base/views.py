0e15e361eb6de0f2e5ad3f8c873978861994067f
Girban Adhikari <adgirban1@gmail.com>
2024-04-21 19:03:40 +0545

    models/migrations added

--- girban/base/views.py
+++ girban/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Work
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    works = Work.objects.all()
+    return render(request, 'base/home.html', {'works': works})
 
 def about(request):
     return render(request, 'base/about.html')
